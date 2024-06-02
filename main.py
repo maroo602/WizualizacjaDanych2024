3d001afbb5b96e11b2cc33175e76a36b0aded578
maroo <122287678+maroo602@users.noreply.github.com>
2024-06-02 10:43:06 +0200

    02.06

--- main.py
+++ main.py
@@ -1,125 +1,106 @@
-# a = 56
-# print(a)
-# print(type(a))
-# b = 5.5
-# print(b)
-# print(type(b))
-# zmienna_tekstowa = 'wizualizacja danych'
-# print(zmienna_tekstowa)
-# print(type(zmienna_tekstowa))
-
-
-#a = 6.55
-#b = 3
-
-#c = a + b
-#print(c)
-#d = a - b
-#print(d)
-#e = 4
-#f = b // a
-#print(f)
-#f = a // b
-#print(f)
-#g = a ** 2
-#print(g)
-#h = pow(a, 2)
-#print(h)
-
-#i = 6 ** (1/2)
-#j = pow(6, 1 / 2)
-#print(i)
-#print(j)
-
-#k= 'wizualizacja danych '
-#l='grupa '
-#m=1
-#print(k + l + str(m))
-#print('Liczba a jest rowna {:.2f}, liczba b jest rowna {:d}'.format(a, b))
-# import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
 #
-# a=input('Wprowadz liczbe: ')
-# print(a + ' to wartosc liczby a')
-# print(type(a))
-# a= int(a)
-# print(a*5)
-# print(type(a))
+# plt.plot([1,2,3,4])
+# plt.ylabel('jakies liczby')
+# plt.show()
+
+# plt.plot([1,2,3,4],[1,4,9,16],'ro-')
+# plt.axis((0,6,0,20))
+# plt.show()
+# plt.plot([1,2,3,4],[1,4,9,16],'b')
+# plt.plot([1,2,3,4], [1,4,9,16], 'o')
+# plt.axis((0,6,0,20))
+# plt.show()
+# x=np.linspace(0,2,100)
+# plt.plot(x,x,label="liniowa")
+# plt.plot(x,x**2,label="kwadratowa")
+# plt.plot(x,x**3, label="sześcienna")
+# plt.xlabel('etykieta x')
+# plt.ylabel('etykieta y')
+# plt.title("Prosty wykres")
+# plt.legend(labels=['liniowa'],'kwadratowa'])
+# plt.savefig('wykres matplot.png')
+# plt.show()
+# im1 = Image.open('wykres matplot.png')
+# im1=im1.convert('RGB')
+# im1.save('nowy.jpg')
+
 #
-# sys.stdout.write('Wprowadz liczbe: ')
-# b= sys.stdin.readline()
-# print(b)
-# print(type(b))
+# x=np.arange(0,10,0.1)
+# s=np.sin(x)
+# plt.plot(x,s,label='sin(x)')
+# plt.xlabel('x')
+# plt.ylabel('sin(x)')
+# plt.title('Wykres sin(x)')
+# plt.legend()
+# plt.show()
 
-lista = [5, 6.6, 34, 'a', 'b', [2, 3, 4], 'ab']
-# print(lista)
-# lista.append(67)
-# print(lista)
-# lista.insert(2, 'c')
-# print(lista)
-# lista.extend([20, 21, 22])
-# print(lista)
-# lista.pop()
-# print(lista)
-# lista.pop(2)
-# print(lista)
-# lista.remove([2,3,4])
-# print(lista)
-# del lista[1]
-# print(lista)
-# #del lista
-# lista.reverse()
-# print(lista)
-# lista.sort()
-# print(lista)
+# data={'a': np.arange(50),
+#       'c': np.random.randint(0,50,50),
+#       'd': np.random.randn(50)}
+# data['b']=data['a']+10*np.random.randn(50)
+# data['d']=np.abs(data['d'])*100
+# print(f"a={data['a'][0]}, b={data['b'][0]}, c={data['c'][0]},d={data['d'][0]}")
+# plt.scatter('a','b', c='c', cmap='magma', s='d', data=data)
+# plt.show()
 
-# slownik = {'klucz': 'wartosc', 1: 2, 'a': 5, 4: 'b'}
-# print(slownik)
-# print(slownik[4])
-# slownik[6]= 45
-# print(slownik)
-# slownik.pop(1)
-# print(slownik)
-# print(slownik.keys())
-# print(slownik.values())
-# del slownik[6]
-# print(slownik)
+# x1=np.arange(0,2,0.02)
+# x2=np.arange(0,2,0.02)
+# y1=np.sin(2*np.pi*x1)
+# y2=np.cos(2*np.pi*x2)
+# plt.subplot(2,1,1)
+# plt.plot(x1,y1,'-')
+# plt.title('wykres sin(x)')
+# plt.xlabel('x')
+# plt.ylabel('sin(x)')
+#
+# plt.subplot(2,1,2)
+# plt.plot(x2,y2,'r-')
+# plt.xlabel('x')
+# plt.ylabel('cos(x)')
+# plt.title('wykres cos(x)')
+# plt.subplots_adjust(hspace=0.5, left=0.1)
+# plt.show()
 
-# a=8
-# b=8
-# if a>b:
-#     print("a is greater than b")
-# elif a<b:
-#     print("a is less than b")
-# else:
-#     print("a is equal to b")
+# x1=np.arange(0.0, 2.0, 0.02)
+# x2=np.arange(0.0,2.0,0.02)
+# y1=np.sin(2*np.pi*x1)
+# y2=np.cos(2*np.pi*x2)
+# fig, axs = plt.subplots(3,2)
+# plt.subplots_adjust(left=0.1)
+# axs[0,0].plot(x1,y1,'-')
+# axs[0,0].set_title('wykres sin(x)')
+# axs[0,0].set_xlabel('x')
+# axs[0,0].set_ylabel('sin(x)')
+# axs[1,1].plot(x2,y2,'r-')
+# axs[1,1].set_title('wykres cos(x)')
+# axs[1,1].set_xlabel('x')
+# axs[1,1].set_ylabel('cos(x)')
+# axs[2,0].plot(x2,y2,'r-')
+# axs[2,0].set_title('wykres cos(x)')
+# axs[2,0].set_xlabel('x')
+# axs[2,0].set_ylabel('cos(x)')
+# fig.delaxes(axs[0,1])
+# fig.delaxes(axs[1,0])
+# fig.delaxes(axs[2,1])
+# plt.show()
 
-    # a = int(input())
-    # b = int(input())
-    # c = int(input())
-    # d = int(input())
-    #
-    # if (a>b) & (c>d):
-    #     print(a, c)
-    # else:
-    #     print(b, d)
-
-# for i in range(1, 8, 2):
-#     print(i)
-# else:
-#     print('koniec petli')
-
-# for i in lista:
-#     print(i)
-
-# for i in range(0,5):
-#     for j in range(0,5):
-#         result=i+j
-#         print(result)
-#     print('')
-
-licznik = 0
-while licznik < len(lista):
-    print(lista[licznik])
-    licznik += 1
-else:
-    print('koniec listy')
+data={'Kraj': ['Belgia', 'Indie', 'Brazylia', 'Polska'],
+      'Stolica': ['Bruksela', 'New Delhi', 'Brasilia', 'Warszawa'],
+      'Kontynent': ['Europa', 'Azja', 'Ameryka Południowa', 'Europa'],
+      'Populacja': [11190846,1303171035,207847528,38675467]}
+df=pd.DataFrame(data)
+print(df)
+grupa=df.groupby('Kontynent')
+etykiety=list(grupa.groups.keys())
+wartosci=list(grupa.agg('Populacja').sum())
+fig, ax = plt.subplots()
+ax.bar(x=etykiety, height=wartosci, color=['yellow', 'green', 'red'])
+ax.set_xlabel('Kontynenty')
+ax.set_ylabel('Populacja w mld')
+ax.ticklabel_format(axis='y', style='plain')
+fig.subplots_adjust(left=0.2)
+plt.show()
